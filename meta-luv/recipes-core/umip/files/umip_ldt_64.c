999e5c5616c862c376606009f1cc8202e803df04
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2017-10-20 15:12:23 -0700

    umip_ldt_64: preserve %r[8-15] registers
    Some local variables are tracked using the %r[8-15] registers. Thus,
    we need to restore their value after testing. Otherwise, errors such
    as segmentation faults can be observed.
    
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

--- meta-luv/recipes-core/umip/files/umip_ldt_64.c
+++ meta-luv/recipes-core/umip/files/umip_ldt_64.c
@@ -143,12 +143,28 @@ int main(void)
 	    "push %%rdi\n\t"
 	    "push %%rsi\n\t"
 	    "push %%rbp\n\t"
+	    "push %%r8\n\t"
+	    "push %%r9\n\t"
+	    "push %%r10\n\t"
+	    "push %%r11\n\t"
+	    "push %%r12\n\t"
+	    "push %%r13\n\t"
+	    "push %%r14\n\t"
+	    "push %%r15\n\t"
 	    /* set new data segment */
             /* jump to test code */
 	    "call *%2\n\t"
 	    /* After running tests, we return here */
             "finish_testing:\n\t"
 	    /* restore everything */
+	    "pop %%r15\n\t"
+	    "pop %%r14\n\t"
+	    "pop %%r13\n\t"
+	    "pop %%r12\n\t"
+	    "pop %%r11\n\t"
+	    "pop %%r10\n\t"
+	    "pop %%r9\n\t"
+	    "pop %%r8\n\t"
 	    "pop %%rbp\n\t"
 	    "pop %%rsi\n\t"
 	    "pop %%rdi\n\t"
